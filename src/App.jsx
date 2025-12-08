e0a437937fd64d422ad1312bcca1e4a88be65d5d
Ahmed <hikalahmed10@gmail.com>
2025-12-08 04:53:03 +0200

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen w-full relative bg-white">
+    <div className="flex flex-col min-h-screen w-full relative bg-blue-50">
       <Header />
       <main className="mt-14">
         <Outlet />
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Navmobile from './Navmobile'
 
 const Header = () => {
     return (
         <header>
             <Navbar />
+            <Navmobile />
         </header>
     )
 }
