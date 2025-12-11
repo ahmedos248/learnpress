ae3bbe5a6cee47827efb00c2b9daa8456dbf5be9
Ahmed <hikalahmed10@gmail.com>
2025-12-11 05:29:40 +0200

    added Courses page and cards

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 import Navmobile from './Navmobile'
 
+
 const Header = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+
     return (
         <header>
-            <Navbar />
+            <Navbar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
             <Navmobile />
         </header>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
-import Home from './pages/Home.jsx'
-
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import Blog from './pages/Blog.jsx';
+import BlogSingle from './pages/BlogSingle.jsx';
+import Courses from './pages/Courses.jsx';
+import CourseDetails from './pages/CourseDetails.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
       { index: true, element: <Home /> },
+      { path: "blog", element: <Blog /> },
+      { path: "blog/:id", element: <BlogSingle /> },
+      { path: "courses", element: <Courses /> },
+      { path: "courses/:id", element: <CourseDetails /> },
+
     ],
-  }
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
